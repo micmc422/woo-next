0d29336ea0a565f518b0d55ea4d84d8ad013cf51
micmc422 <micmc422@gmail.com>
2021-06-22 21:52:53 +0200

    update

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,12 @@ import "../src/styles/style.scss";
 import "../src/styles/main.scss";
 import "tailwindcss/tailwind.css";
 
-import Router from "next/router";
+import Router, { useRouter } from "next/router";
 import NProgress from "nprogress";
 import { appWithTranslation } from "next-i18next";
 import CookieConsent from "react-cookie-consent";
+import * as ga from '../src/lib/ga'
+import { useEffect } from "react";
 
 NProgress.configure({ showSpinner: true });
 Router.events.on("routeChangeStart", () => NProgress.start());
@@ -16,6 +18,24 @@ export function reportWebVitals(metric) {
 }
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+
   return (
     <>
       <CookieConsent
